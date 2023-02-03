effa631394eacdf4b2260887594f702c2642abd9
siannas <massive.annas@gmail.com>
2023-02-03 10:58:02 +0700

    Updates

--- sw.js
+++ sw.js
@@ -27,7 +27,7 @@ workbox.core.clientsClaim();
  */
 self.__precacheManifest = [
   {
-    "url": "webpack-runtime-c2a5c0bf1fbe0f588cca.js"
+    "url": "webpack-runtime-fa0619f61fc79bdcbb2b.js"
   },
   {
     "url": "styles.7ffa7536ff2e5ef0c665.css"
@@ -40,7 +40,7 @@ self.__precacheManifest = [
   },
   {
     "url": "offline-plugin-app-shell-fallback/index.html",
-    "revision": "2221cc837c1ec5765052a33329eed985"
+    "revision": "7020bef987f7717b765b0abb6a03eb4f"
   },
   {
     "url": "component---cache-caches-gatsby-plugin-offline-app-shell-js-725b74cf1fe5129ae7a9.js"
@@ -51,7 +51,7 @@ self.__precacheManifest = [
   },
   {
     "url": "page-data/app-data.json",
-    "revision": "740cf0d7af37e81d7b954ad8cea083bf"
+    "revision": "fdccdd828b507e6e348f9c76147311e2"
   },
   {
     "url": "polyfill-1b6a08d020871bce2120.js"
